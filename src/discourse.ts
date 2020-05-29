fe18d3b6cc1cc502fc63e73320ad408691b5be2a
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-05-29 13:00:35 -0700

    Build basic query infrastructure

---
+++ src/discourse.ts
@@ -0,0 +1,53 @@
+import fetch from 'node-fetch'
+
+interface Colrender {
+  [key: string]: string
+}
+
+interface Params {
+  [key: string]: string
+}
+
+interface Relations {
+  [key: string]: object[]
+}
+
+interface QueryResponse {
+  success: boolean
+  errors: string[]
+  duration: number
+  result_count: number
+  params: Params
+  columns: string[]
+  default_limit: number
+  relations: Relations
+  colrender: Colrender
+  rows: Array<Array<any>>
+}
+
+export async function executeQuery(
+  hostname: string,
+  id: string | number,
+  paramsText: string,
+  key: string
+): Promise<QueryResponse> {
+  const params = JSON.parse(paramsText)
+  const url = `https://${hostname}/admin/plugins/explorer/queries/${id}/run`
+  const body = JSON.stringify({ params: JSON.stringify(params) })
+
+  const response = await fetch(url, {
+    body: body,
+    headers: {
+      'api-key': key,
+      'api-username': 'system',
+      'content-type': 'application/json; charset=UTF-8'
+    },
+    method: 'POST'
+  })
+
+  if (response.status != 200) {
+    throw new Error(`${response.status} ${response.statusText}`)
+  }
+
+  return response.json() as Promise<QueryResponse>
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,35 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+
+import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const discourseKey = core.getInput('discourseKey', { required: true })
+    const hostname = core.getInput('hostname', { required: true })
+    const id = core.getInput('id', { required: true })
+    const rawParams = core.getInput('params')
+    const params = rawParams ? JSON.parse(rawParams) : {}
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const url = `https://${hostname}/admin/plugins/explorer/queries/${id}/run`
+    const body = JSON.stringify({ params: JSON.stringify(params) })
 
-    core.setOutput('time', new Date().toTimeString())
+    const response = await fetch(url, {
+      body: body,
+      headers: {
+        'api-key': discourseKey,
+        'api-username': 'system',
+        'content-type': 'application/json; charset=UTF-8'
+      },
+      method: 'POST'
+    })
+
+    if (response.status != 200) {
+      throw new Error(`${response.status} ${response.statusText}`)
+    }
+
+    const json = await response.json()
+
+    core.setOutput('results', JSON.stringify(json))
   } catch (error) {
     core.setFailed(error.message)
   }
