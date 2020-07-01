880cc857f42a236c172f875cc7001b8a2487523f
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-07-01 14:02:43 -0700

    Enable storing the results as a Markdown table

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 A GitHub Action to execute a Discourse query and return the results.
 
+## Use
+
+### Inputs
+
+- `discourseKey` &mdash; **required** API key used to execute the query
+- `format` &mdash; The format to store the results in: either `json` or `markdown` (_default:_ `json`)
+- `hostname` &mdash; **required** Hostname of the Discourse instance to submit the query to
+- `id` &mdash; **required** ID of the query to execute
+- `params` &mdash; JSON object containing the parameters for the query
+- `path` &mdash; **required** File path to store the results in
+
+### Outputs
+
+- `path` &mdash; File path where the results of the query were stored
+
 ## License
 
 [MIT](LICENSE.md)
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,57 @@
+import fs from 'fs'
+import util from 'util'
+
 import * as core from '@actions/core'
 
-import { executeQuery } from './discourse'
+import { executeQuery, resultsToTable } from './discourse'
+import { fstat } from 'fs'
+
+const writeFile = util.promisify(fs.writeFile)
+
+enum Format {
+  JSON = 'json',
+  MARKDOWN = 'markdown'
+}
 
 async function run(): Promise<void> {
   try {
     const discourseKey = core.getInput('discourseKey', { required: true })
+    const format = validateFormat(core.getInput('format') ?? 'json')
     const hostname = core.getInput('hostname', { required: true })
     const id = core.getInput('id', { required: true })
     const rawParams = core.getInput('params')
     const params = rawParams ? JSON.parse(rawParams) : {}
+    const path = core.getInput('path', { required: true })
 
     const json = await executeQuery(hostname, id, params, discourseKey)
 
-    core.setOutput('results', JSON.stringify(json))
+    switch (format) {
+      case Format.JSON:
+        await writeFile(path, json)
+        break;
+
+      case Format.MARKDOWN:
+        const table = await resultsToTable(json)
+        await writeFile(path, table)
+        break;
+    }
+
+    core.setOutput('path', path)
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+function validateFormat(format: string): Format {
+  if (!(format in Format)) {
+    throw new Error(
+      `'${format}' is not a valid output format, please select one of: ${Object.values(Format).join(
+        ', '
+      )}`
+    )
+  }
+
+  return format as Format
 }
 
 run()
